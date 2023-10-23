2f907acb15521f77b169fffd79ef4f314b840310
Andrey O. <ozzy@triasoft.com>
2023-10-24 02:55:09 +0300

    Add checks when startup types extract

--- src/MyLab.Task.Runtime/Tools/TaskAssetExtractor.cs
+++ src/MyLab.Task.Runtime/Tools/TaskAssetExtractor.cs
@@ -30,25 +30,37 @@ public class TaskAssetExtractor
         
         var found = _strategy.GetAssemblyTypes(assembly)
             .Where(t => t.IsPublic && t.IsImplTaskStartup)
-            .Select(t => new TaskFactory
-            (
-                new TaskQualifiedName(_taskAssetSource.Name, t.Name),
-                t.Type
-            ))
             .ToArray();
 
         if(found.Length == 0) 
         {
-            throw new InvalidOperationException("No tasks found")
-                .AndFactIs("assert", _taskAssetSource.Name);
+            throw new InvalidOperationException("No tasks found");
         }
-        if(found.Count(t => string.IsNullOrWhiteSpace(t.Name.LocalName)) > 1)
+        if(found.Count(t => string.IsNullOrWhiteSpace(t.Name)) > 1)
         {
-            throw new InvalidOperationException("More then one task with default name detected")
-                .AndFactIs("assert", _taskAssetSource.Name);
+            throw new InvalidOperationException("More then one task with default name detected");
         } 
 
-        return found;
+        foreach(var t in found)
+        {
+            if(t.HasGenericParams)
+            {
+                throw new InvalidOperationException("Generic startup types is not supported")
+                    .AndFactIs("type", t.Type.FullName);
+            }
+            if(!t.HasPubDefCtor)
+            {
+                throw new InvalidOperationException("Startup type has no default public constructor")
+                    .AndFactIs("type", t.Type.FullName);
+            }
+        }
+
+        return found.Select(t => new TaskFactory
+            (
+                new TaskQualifiedName(_taskAssetSource.Name, t.Name),
+                t.Type
+            )
+        );
     }
 
     class DefaultStrategy : IStrategy
@@ -64,7 +76,15 @@ public class TaskAssetExtractor
         IEnumerable<TypeDesc> GetAssemblyTypes(Assembly assembly);
     }
 
-    public record TypeDesc(Type Type, bool IsPublic, bool IsImplTaskStartup, string? Name)
+    public record TypeDesc
+    (
+        Type Type, 
+        bool IsPublic, 
+        bool IsImplTaskStartup,
+        bool HasGenericParams, 
+        bool HasPubDefCtor, 
+        string? Name
+    )
     {
         public static TypeDesc FromType(Type type)
         {
@@ -73,6 +93,8 @@ public class TaskAssetExtractor
                 type,
                 type.IsPublic,
                 type.IsAssignableTo(typeof(ITaskStartup)),
+                type.GetGenericArguments().Length != 0,
+                type.GetConstructors().Any(c => c.IsPublic && c.GetParameters().Length == 0),
                 type.GetCustomAttribute<TaskNameAttribute>()?.Name
             );
         }
--- src/UnitTests/TaskAssetExtractorBehavior.cs
+++ src/UnitTests/TaskAssetExtractorBehavior.cs
@@ -47,6 +47,8 @@ public partial class TaskAssetExtractorBehavior
                     Type: typeof(TestTaskStartup), 
                     IsPublic: true,
                     IsImplTaskStartup: true,
+                    HasGenericParams: false,
+                    HasPubDefCtor: true,
                     Name: null
                 ),
                 new TypeDesc
@@ -54,6 +56,8 @@ public partial class TaskAssetExtractorBehavior
                     Type: typeof(TestTaskStartup), 
                     IsPublic: true,
                     IsImplTaskStartup: true,
+                    HasGenericParams: false,
+                    HasPubDefCtor: true,
                     Name: null
                 ),
             }
@@ -80,6 +84,8 @@ public partial class TaskAssetExtractorBehavior
                     Type: typeof(TestTaskStartup), 
                     IsPublic: true,
                     IsImplTaskStartup: true,
+                    HasGenericParams: false,
+                    HasPubDefCtor: true,
                     Name: localName
                 ),
             }
@@ -95,6 +101,95 @@ public partial class TaskAssetExtractorBehavior
         Assert.NotNull(foundTask?.Name);
         Assert.Equal("foo", foundTask.Name.Asset);
         Assert.Equal(localName, foundTask.Name.LocalName);
+    }
+
+    [Fact(DisplayName = "Ignore non-public startups")]
+    public void ShouldIgnoreNonPublicStartups()
+    {
+        //Arrange        
+        var getAssemblyTypesMock = GetStrategyMock
+        (
+            new []
+            {
+                new TypeDesc
+                (
+                    Type: typeof(TestTaskStartup), 
+                    IsPublic: true,
+                    IsImplTaskStartup: true,
+                    HasGenericParams: false,
+                    HasPubDefCtor: true,
+                    Name: "foo"
+                ),
+                new TypeDesc
+                (
+                    Type: typeof(TestTaskStartup), 
+                    IsPublic: false,
+                    IsImplTaskStartup: true,
+                    HasGenericParams: false,
+                    HasPubDefCtor: true,
+                    Name: "bar"
+                ),
+            }
+        );
+        
+        var extractor = new TaskAssetExtractor(_testAsset, getAssemblyTypesMock);
+        
+        //Act 
+        var tasks = extractor.Extract().ToArray();
+        var foundTask = tasks.SingleOrDefault();
+
+        //Assert
+        Assert.Equal("foo", foundTask?.Name.LocalName);
+    }
+
+    [Fact(DisplayName = "Fail if generic startup")]
+    public void ShoudFailIfGenericStartup()
+    {
+        //Arrange
+        var getAssemblyTypesMock = GetStrategyMock
+        (
+            new []
+            {
+                new TypeDesc
+                (
+                    Type: typeof(TestTaskStartup), 
+                    IsPublic: true,
+                    IsImplTaskStartup: true,
+                    HasGenericParams: true,
+                    HasPubDefCtor: true,
+                    Name: null
+                ),
+            }
+        );
+        var extractor = new TaskAssetExtractor(_testAsset, getAssemblyTypesMock);
+        
+        //Act & Assert
+        Assert.Throws<InvalidOperationException>(() => extractor.Extract().ToArray());
+    }
+
+    [Fact(DisplayName = "Fail if has no public default constructor")]
+    public void ShoudFailIfHasNoPubDefCtor()
+    {
+        //Arrange
+        var getAssemblyTypesMock = GetStrategyMock
+        (
+            new []
+            {
+                new TypeDesc
+                (
+                    Type: typeof(TestTaskStartup), 
+                    IsPublic: true,
+                    IsImplTaskStartup: true,
+                    HasGenericParams: false,
+                    HasPubDefCtor: false,
+                    Name: null
+                ),
+            }
+        );
+        var extractor = new TaskAssetExtractor(_testAsset, getAssemblyTypesMock);
+        
+        //Act & Assert
+        Assert.Throws<InvalidOperationException>(() => extractor.Extract().ToArray());
     }
 
 }
