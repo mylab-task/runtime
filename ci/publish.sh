5027925ba94b30fd79920ad9c633efd59c6e812b
Andrey O. <ozzy.ext@gmail.com>
2025-11-28 10:32:23 +0300

    Пправил публикацию

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,4 +1,3 @@
-docker build --platform linux/amd64 -f ./Dockerfile -t ghcr.io/mylab-task/runtime:latest -t ghcr.io/mylab-task/runtime:$1 ..
-
+docker build --platform linux/amd64 -f ./Dockerfile -t ghcr.io/mylab-task/runtime:latest -t ghcr.io/mylab-task/runtime:$1 .. 
 docker push ghcr.io/mylab-task/runtime:$1
 docker push ghcr.io/mylab-task/runtime:latest
